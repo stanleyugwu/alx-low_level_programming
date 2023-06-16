a160e59613b551edf82f032c02ff5d9e42b42a07
Stanley <stanleyugwu2018@gmail.com>
2023-06-16 05:01:10 +0100

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/* Displays a text to standard error */
+
+#include <stdio.h>
+
+/**
+ * main - Main entry point
+ *
+ * Program's entry
+ *
+ * Return: returns exit code 0
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}

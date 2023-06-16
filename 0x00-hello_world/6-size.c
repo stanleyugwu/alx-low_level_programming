4d16b33cb1aaddcc15c30fd57437d2ee005e8b60
Stanley <stanleyugwu2018@gmail.com>
2023-06-16 04:10:58 +0100

    fix wrap bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", sizeof char);
-	printf("Size of a int: %d byte(s)", sizeof int);
-	printf("Size of a long: %d byte(s)", sizeof long);
-	printf("Size of a long long: %d byte(s)", sizeof long long);
-	printf("Size of a float: %d byte(s)", sizeof float);
+	printf("Size of a char: %d byte(s)", sizeof (char));
+	printf("Size of a int: %d byte(s)", sizeof (int));
+	printf("Size of a long: %d byte(s)", sizeof (long));
+	printf("Size of a long long: %d byte(s)", sizeof (long long));
+	printf("Size of a float: %d byte(s)", sizeof (float));
 }
